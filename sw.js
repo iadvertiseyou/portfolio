314c11376f0cc7401f3e39dd866c8fc48b8ba928
iadvertiseyou <61354849+iadvertiseyou@users.noreply.github.com>
2020-03-23 20:40:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,30 @@
+const staticCacheName = "staticCache";
+const staticAssets = [
+  "/",
+  "/index.html",
+  "/scripts/main.js",
+  "/styles/style.css",
+  "/assets/david-carbon-webdesign-logo.svg",
+  "/assets/bg.jpg",
+  "/assets/angle-arrow-down.svg",
+  "https://fonts.googleapis.com/css?family=Dosis:300,500,800&display=swap"
+];
+
 self.addEventListener("install", e => {
-  console.log("installled");
+  e.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      cache.addAll(staticAssets);
+    })
+  );
 });
 
 self.addEventListener("activate", e => {
-  console.log("activated");
+  //
 });
 self.addEventListener("fetch", e => {
-  console.log("Feteched", e);
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
